2c69db5729128f040e9d959f2f0ae475d4a70089
susgri <susanne@gripsgaard.no>
2025-01-12 17:54:04 +0100

    task 2 done

---
+++ Exercise1/bounded buffer/go/main.go
@@ -0,0 +1,42 @@
+
+package main
+
+import "fmt"
+import "time"
+
+
+func producer(ch chan int){
+
+    for i := 0; i < 10; i++ {
+        time.Sleep(100 * time.Millisecond)
+        fmt.Printf("[producer]: pushing %d\n", i)
+        // TODO: push real value to buffer
+        ch <- i
+    }
+
+}
+
+func consumer(ch chan int){
+
+    time.Sleep(1 * time.Second)
+    for {
+        i := <- ch //TODO: get real value from buffer
+
+        fmt.Printf("[consumer]: %d\n", i)
+        time.Sleep(50 * time.Millisecond)
+    }
+    
+}
+
+
+func main(){
+    
+    // TODO: make a bounded buffer
+
+    ch := make(chan int, 5)
+    
+    go consumer(ch)
+    go producer(ch)
+    
+    select {}
+}
